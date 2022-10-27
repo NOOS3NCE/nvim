8c6ff1db8858ae9e5dc5adb3d0e904a0590b2399
Michael Cummings <halemikehale@gmail.com>
2022-10-26 22:07:06 -0500

    Started setup script, and research for lvim defaults

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# INSTALL PROGRAMS
+# __STARSHIP__
+# theme for zsh -- https://starship.rs/
+# curl -sS https://starship.rs/install.sh | sh
+# 
+# add the following to the end of .zshrc file
+# eval "$(starship init zsh)"
+#
+# -write config file-
+# 
+# ____________________________________________________
+# __oh-my-zsh__
+#
+# install nvim nightly
+# 
